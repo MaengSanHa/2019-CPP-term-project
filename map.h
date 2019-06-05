823c86b2132122a221ca6950732833b3b9597045
MaengSanHa <msh0117@kookmin.ac.kr>
2019-06-06 02:39:32 +0900

    upload map.h and pushBoxMap.hpp 20190606
    modify map.h and pushBoxMap.hpp
    adding default map and game logic

--- map.h
+++ map.h
@@ -11,7 +11,7 @@ using namespace std;
 6 represents box on destination
 */
 
-// interface for push box map
+// interface for push box map and game logic
 
 const int NULL_SPACE=0, WALL=1, BOX=2, DEST=3, OUTSIDE=4, CHARACTER = 5, BOX_ON_DEST=6;
 
@@ -19,9 +19,15 @@ class Map
 {
     public:
         vector<vector<int>> vec;
+        vector<vector<int>> default_map; // default value of map
+        static vector<int> location_of_character;
 
         // Constructor
         Map(vector<vector<int>> vector);
 
         void setElement(int x, int y, int input);
+        void setCharacter(int x, int y); // charactor represent
+        void move(char arrow);
+        void find_character();
+        int do_nothing();
 };
