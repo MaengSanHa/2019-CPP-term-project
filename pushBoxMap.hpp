07a01deeb4c95cdb0a5fee3a9c67f806f688966f
MaengSanHa <msh0117@kookmin.ac.kr>
2019-06-06 12:47:28 +0900

    modify pushBoxMap.hpp 20190606

--- pushBoxMap.hpp
+++ pushBoxMap.hpp
@@ -59,11 +59,11 @@ void Map::move(char arrow)
     find_character();
     int char_row = location_of_character[0]; // 캐릭터의 현재 위치(행 값)
     int char_col = location_of_character[1]; // 캐릭터의 현재 위치(열 값)
+    int*loc = new int;
+    *loc = vec[char_row][char_col]; // 캐릭터의 현재 위치 동적 할당
     switch(arrow)
     {   
         case 72: // ↑ 입력
-            int*loc = new int;
-            *loc = vec[char_row][char_col]; // 캐릭터의 현재 위치
             if(vec[char_row-1][char_col]==NULL_SPACE) // 캐릭터 위쪽이 비어있으면 캐릭터를 위쪽으로 한칸 전진
             {
                 setCharacter(char_row-1, char_col);
@@ -92,12 +92,9 @@ void Map::move(char arrow)
                     setElement(char_row-2, char_col, BOX_ON_DEST); // 목적지에 박스가 들어갔음을 표시
                 }
             }
-            delete loc;
             break;
         
         case 75: // <- 입력
-            int*loc = new int;
-            *loc = vec[char_row][char_col]; // 캐릭터의 현재 위치
             if(vec[char_row][char_col-1]==NULL_SPACE) // 캐릭터 왼쪽이 비어있으면 캐릭터를 왼쪽으로 한칸 전진
             {
                 setCharacter(char_row, char_col-1);
@@ -126,12 +123,9 @@ void Map::move(char arrow)
                     setElement(char_row, char_col-2, BOX_ON_DEST); // 목적지에 박스가 들어갔음을 표시
                 }
             }
-            delete loc;
             break;
 
         case 77: // -> 입력
-            int *loc = new int;
-            *loc = vec[char_row][char_col]; // 캐릭터의 현재 위치
             if(vec[char_row][char_col+1]==NULL_SPACE) // 캐릭터 오른쪽이 비어있으면 캐릭터를 오른쪽으로 한칸 전진
             {
                 setCharacter(char_row, char_col+1);
@@ -160,12 +154,9 @@ void Map::move(char arrow)
                     setElement(char_row, char_col+2, BOX_ON_DEST); // 목적지에 박스가 들어갔음을 표시
                 }
             }
-            delete loc;
             break;
         
         case 80: // ↓ 입력
-            int*loc = new int;
-            *loc = vec[char_row][char_col]; // 캐릭터의 현재 위치
             if(vec[char_row+1][char_col]==NULL_SPACE) // 캐릭터 아래쪽이 비어있으면 캐릭터를 아래쪽으로 한칸 전진
             {
                 setCharacter(char_row+1, char_col);
@@ -194,8 +185,8 @@ void Map::move(char arrow)
                     setElement(char_row+2, char_col, BOX_ON_DEST); // 목적지에 박스가 들어갔음을 표시
                 }
             }
-            delete loc;
             break;
+        delete loc;
             
     }
 }
