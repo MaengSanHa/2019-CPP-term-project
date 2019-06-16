e9c7de75bbda3831d50dba992a2140279e1aef17
MaengSanHa <msh0117@kookmin.ac.kr>
2019-06-16 18:09:54 +0900

    upload Map.cpp
    add numStep, numPush, increaseNumStep(), increaseNumPush()

--- Map.cpp
+++ Map.cpp
@@ -13,6 +13,7 @@ Map::Map(){
 /*  
 복사 생성자는 main()에서 선언된 2차원 배열 map_of_stage를 map과 default_map에 복사
 목적지의 갯수를 numDest에 저장
+Step 횟수와 Push 횟수를 0으로 초기화
 */
 Map::Map(int map_of_stage[10][10]){
     for (int i=0; i<10; ++i){
@@ -22,6 +23,8 @@ Map::Map(int map_of_stage[10][10]){
         }
     }
     countDest();
+    numStep = 0;
+    numPush = 0;
 }
 
 // map의 (target_x, target_y) 위치의 값을 input으로 변경
@@ -58,6 +61,7 @@ Map& Map::move(char arrow){
             if ((this->map[char_row-1][char_col] == 0)||(this->map[char_row-1][char_col] == 3)){ // 캐릭터 위쪽이 비어있거나 목적지이면 캐릭터를 위쪽으로 한칸 전진
                 setCharacter(char_row-1, char_col);
                 setElement(char_row, char_col, default_map[char_row][char_col]);
+                increaseNumStep();
             }
             else if (this->map[char_row-1][char_col] == 1){ // 캐릭터 위쪽이 벽이면
                 do_nothing(); // 캐릭터 이동 불가능
@@ -70,86 +74,105 @@ Map& Map::move(char arrow){
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row-1, char_col); // 캐릭터와 박스가 함께 위쪽으로 이동
                     setElement(char_row-2, char_col, 2);
+                    increaseNumStep();
+                    increaseNumPush();
                 }
                 else if (this->map[char_row-2][char_col] == 3){ // 박스 앞이 목적지이면               
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row-1, char_col); // 캐릭터와 박스가 함께 위쪽으로 이동
                     setElement(char_row-2, char_col, 6); // 목적지에 박스가 들어갔음을 표시
+                    increaseNumStep();
+                    increaseNumPush();
                 }
             }
             break;
         
         case 75 : // <- 입력
-            if ((map[char_row][char_col-1] == 0)||(map[char_row][char_col-1] == 3)){ // 캐릭터 왼쪽이 비어있거나 목적지이면 캐릭터를 왼쪽으로 한칸 전진
+            if ((this->map[char_row][char_col-1] == 0)||(this->map[char_row][char_col-1] == 3)){ // 캐릭터 왼쪽이 비어있거나 목적지이면 캐릭터를 왼쪽으로 한칸 전진
                 setCharacter(char_row, char_col-1);
                 setElement(char_row, char_col, default_map[char_row][char_col]);
+                increaseNumStep();
             }
-            else if (map[char_row][char_col-1] == 1){ // 캐릭터 왼쪽이 벽이면
+            else if (this->map[char_row][char_col-1] == 1){ // 캐릭터 왼쪽이 벽이면
                 do_nothing(); // 캐릭터 이동 불가능
             }
-            else if (map[char_row][char_col-1] == 2){ // 캐릭터 왼쪽이 박스일 때
-                if ((map[char_row][char_col-2] == 1)||(map[char_row][char_col-2] == 2)){ // 박스 왼쪽이 벽이거나 박스이면
+            else if (this->map[char_row][char_col-1] == 2){ // 캐릭터 왼쪽이 박스일 때
+                if ((this->map[char_row][char_col-2] == 1)||(this->map[char_row][char_col-2] == 2)){ // 박스 왼쪽이 벽이거나 박스이면
                     do_nothing(); // 캐릭터 이동 불가능
                 }
-                else if (map[char_row][char_col-2] == 0){ // 박스 앞이 비어있으면
+                else if (this->map[char_row][char_col-2] == 0){ // 박스 앞이 비어있으면
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row, char_col-1); // 캐릭터와 박스가 함께 왼쪽으로 이동
                     setElement(char_row, char_col-2, 2);
+                    increaseNumStep();
+                    increaseNumPush();
                 }
-                else if (map[char_row][char_col-2] == 3){ // 박스 앞이 목적지이면
+                else if (this->map[char_row][char_col-2] == 3){ // 박스 앞이 목적지이면
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row, char_col-1); // 캐릭터와 박스가 함께 왼쪽으로 이동
                     setElement(char_row, char_col-2, 6); // 목적지에 박스가 들어갔음을 표시
+                    increaseNumStep();
+                    increaseNumPush();
                 }
             }
             break;
 
         case 77 : // -> 입력
-            if ((map[char_row][char_col+1] == 0)||(map[char_row][char_col+1] == 3)){ // 캐릭터 오른쪽이 비어있거나 목적지이면 캐릭터를 오른쪽으로 한칸 전진
+            if ((this->map[char_row][char_col+1] == 0)||(this->map[char_row][char_col+1] == 3)){ // 캐릭터 오른쪽이 비어있거나 목적지이면 캐릭터를 오른쪽으로 한칸 전진
                 setCharacter(char_row, char_col+1);
                 setElement(char_row, char_col, default_map[char_row][char_col]);
+                increaseNumStep();
             }
-            else if (map[char_row][char_col+1] == 1){ // 캐릭터 오른쪽이 벽이면
+            else if (this->map[char_row][char_col+1] == 1){ // 캐릭터 오른쪽이 벽이면
                 do_nothing(); // 캐릭터 이동 불가능
             }
-            else if (map[char_row][char_col+1] == 2){ // 캐릭터 오른쪽이 박스일 때
-                if ((map[char_row][char_col+2] == 1)||(map[char_row][char_col+2] == 2)){ // 박스 오른쪽이 벽이거나 박스이면
+            else if (this->map[char_row][char_col+1] == 2){ // 캐릭터 오른쪽이 박스일 때
+                if ((this->map[char_row][char_col+2] == 1)||(this->map[char_row][char_col+2] == 2)){ // 박스 오른쪽이 벽이거나 박스이면
                     do_nothing(); // 캐릭터 이동 불가능
                 }
-                else if (map[char_row][char_col+2] == 0){ // 박스 앞이 비어있으면
+                else if (this->map[char_row][char_col+2] == 0){ // 박스 앞이 비어있으면
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row, char_col+1); // 캐릭터와 박스가 함께 오른쪽으로 이동
                     setElement(char_row, char_col+2, 2);
+                    increaseNumStep();
+                    increaseNumPush();
                 }
-                else if (map[char_row][char_col+2] == 3){ // 박스 앞이 목적지이면
+                else if (this->map[char_row][char_col+2] == 3){ // 박스 앞이 목적지이면
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row, char_col+1); // 캐릭터와 박스가 함께 오른쪽으로 이동
                     setElement(char_row, char_col+2, 6); // 목적지에 박스가 들어갔음을 표시
+                    increaseNumStep();
+                    increaseNumPush();
                 }
             }
             break;
         
         case 80 : // ↓ 입력
-            if ((map[char_row+1][char_col] == 0)||(map[char_row+1][char_col] == 3)){ // 캐릭터 아래쪽이 비어있거나 목적지이면 캐릭터를 아래쪽으로 한칸 전진
+            if ((this->map[char_row+1][char_col] == 0)||(this->map[char_row+1][char_col] == 3)){ // 캐릭터 아래쪽이 비어있거나 목적지이면 캐릭터를 아래쪽으로 한칸 전진
                 setCharacter(char_row+1, char_col);
                 setElement(char_row, char_col, default_map[char_row][char_col]);
+                increaseNumStep();
             }
-            else if (map[char_row+1][char_col] == 1){ // 캐릭터 아래쪽이 벽이면
+            else if (this->map[char_row+1][char_col] == 1){ // 캐릭터 아래쪽이 벽이면
                 do_nothing(); // 캐릭터 이동 불가능
             }
-            else if (map[char_row+1][char_col] == 2){ // 캐릭터 아래쪽이 박스일 때
-                if ((map[char_row+2][char_col] == 1)||(map[char_row+2][char_col] == 2)){ // 박스 아래쪽이 벽이거나 박스이면
+            else if (this->map[char_row+1][char_col] == 2){ // 캐릭터 아래쪽이 박스일 때
+                if ((this->map[char_row+2][char_col] == 1)||(this->map[char_row+2][char_col] == 2)){ // 박스 아래쪽이 벽이거나 박스이면
                     do_nothing(); // 캐릭터 이동 불가능
                 }
-                else if (map[char_row+2][char_col] == 0){ // 박스 앞이 비어있으면
+                else if (this->map[char_row+2][char_col] == 0){ // 박스 앞이 비어있으면
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row+1, char_col); // 캐릭터와 박스가 함께 아래쪽으로 이동
                     setElement(char_row+2, char_col, 2);
+                    increaseNumStep();
+                    increaseNumPush();
                 }
-                else if (map[char_row+2][char_col] == 3){ // 박스 앞이 목적지이면
+                else if (this->map[char_row+2][char_col] == 3){ // 박스 앞이 목적지이면
                     setElement(char_row, char_col, default_map[char_row][char_col]); // 캐릭터 있던 곳은 기본 값
                     setCharacter(char_row+1, char_col); // 캐릭터와 박스가 함께 아래쪽으로 이동
                     setElement(char_row+2, char_col, 6); // 목적지에 박스가 들어갔음을 표시
+                    increaseNumStep();
+                    increaseNumPush();
                 }
             }
             break;             
@@ -203,5 +226,15 @@ Map& Map::countBoxOnDest(){
             }
         }
     }
+    return *this;
+}
+
+Map& Map::increaseNumStep(){
+    this->numStep++;
+    return *this;
+}
+
+Map& Map::increaseNumPush(){
+    this->numPush++;
     return *this;
 }
